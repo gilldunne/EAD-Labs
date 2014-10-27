7ebbe2cf4c4f4fcd98f4934ab11e90b6540f77c6
gilldunne <x00094469@ittd.ie>
2014-10-27 19:16:33 +0000

    tests

--- Exercise4-SoccerTeam/Exercise4-SoccerTeam/Class1.cs
+++ Exercise4-SoccerTeam/Exercise4-SoccerTeam/Class1.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 
 [assembly: CLSCompliant(true)]
-namespace Exercise4SoccerTeam
-{
-    public enum Gender { Male, Female}
+namespace Exercise4SoccerTeam {
+    public enum Gender { Male, Female }
     public abstract class SportsPlayer {
         public String Name { get; set; }
         public int Age { get; set; }
@@ -20,107 +20,107 @@ namespace Exercise4SoccerTeam
         }
 
         public override String ToString() {
-            return "Name: "+ Name + " \tAge: " + Age + " \tGender: " + Gender;
+            return "Name: " + Name + " \tAge: " + Age + " \tGender: " + Gender;
         }
     }
 
-    public enum Position { Goalkeeper, Defender, Midfielder, Striker}
+    public enum Position { Goalkeeper, Defender, Midfielder, Striker }
     public class SoccerPlayer : SportsPlayer {
         public Position Position { get; set; }
 
         public SoccerPlayer(String name, int age, Gender gender, Position position)
-            : base(name, age, gender){
-                this.Position = position;
+            : base(name, age, gender) {
+            this.Position = position;
         }
 
         public SoccerPlayer()
-            :this("",0,Gender.Male,Position.Defender){
+            : this("", 0, Gender.Male, Position.Defender) {
         }
 
         public override string ToString() {
             return base.ToString() + " \tPosition: " + Position;
         }
+    }
+    public class SoccerTeam : IEnumerable {
+        
+        public const int MinAge = 5;
+        public const int MaxAge = Int32.MaxValue;
+        public String TeamName { get; set; }
+        public Gender TeamGender { get; set; }
 
-        public class SoccerTeam : IEnumerable {
-            private const int minAge = 5;
-            private const int maxAge = Int32.MaxValue;
-            public String TeamName { get; set; }
-            public Gender TeamGender { get; set; }
-
-            private int ageLimit;
-            public int AgeLimit {
-                get {
-                    return ageLimit;
-                }
-                set {
-                    if (value < minAge) {
-                        throw new ArgumentException("Too young to join");
-                    }
-                    else {
-                        this.ageLimit = value;
-                    }
-                }
+        private int ageLimit;
+        public int AgeLimit {
+            get {
+                return ageLimit;
             }
-
-            private List<SoccerPlayer> players;
-
-            public List<SoccerPlayer> Players {
-                get {
-                    return new List<SoccerPlayer>(players);
-                }
-            }
-
-            public SoccerTeam(String teamName, Gender teamGender, int ageLimit) {
-                this.TeamName = teamName;
-                this.TeamGender = teamGender;
-                this.AgeLimit = ageLimit;
-                players = new List<SoccerPlayer>();
-            }
-
-            public IEnumerable GetEnumerator() {
-                foreach (SoccerPlayer s in players) {
-                    yield return s;
-                }
-            }
-            // indexer property
-            public SoccerPlayer this[String playerName] {
-                get {
-                    SoccerPlayer player = null;
-                    bool isFound = false;
-                    for (int i = 0; i < players.Count; i++) {
-                        if(players[i].Name.ToUpper().Equals(playerName.ToUpper())){
-                            isFound = true;
-                            player = players[i];
-                        }
-                    }
-                    if (isFound == true) {
-                        return player;
-                    }
-                    else {
-                        throw new ArgumentException("Player not found");
-                    }
-                }
-            }
-            public void AddPlayer (SoccerPlayer player){
-                if (Players == null) {
-                    players.Add(player);
+            set {
+                if (value < MinAge) {
+                    throw new ArgumentException("Too young to join");
                 }
                 else {
-                    if (players.Contains(player)) {
-                        throw new ArgumentException("Player already on team");
+                    this.ageLimit = value;
+                }
+            }
+        }
+
+        private List<SoccerPlayer> players;
+
+        public Collection<SoccerPlayer> Players {
+            get {
+                return new Collection<SoccerPlayer>(players);
+            }
+        }
+
+        public SoccerTeam(String teamName, Gender teamGender, int ageLimit) {
+            this.TeamName = teamName;
+            this.TeamGender = teamGender;
+            this.AgeLimit = ageLimit;
+            players = new List<SoccerPlayer>();
+        }
+        public IEnumerator GetEnumerator() {
+            foreach (SoccerPlayer s in players) {
+                yield return s;
+            }
+        }
+
+        // indexer property
+        public SoccerPlayer this[String playerName] {
+            get {
+                SoccerPlayer player = null;
+                bool isFound = false;
+                for (int i = 0; i < players.Count; i++) {
+                    if (String.Compare(players[i].Name, playerName, StringComparison.OrdinalIgnoreCase) == 0) {
+                        isFound = true;
+                        player = players[i];
                     }
-                    else {
-                        if (player.Gender == TeamGender) {
-                            if (player.Age <= AgeLimit) {
-                                players.Add(player);
-                            }
-                            else {
-                                throw new ArgumentException("Player too old for team"); 
-                            }
+                }
+                if (isFound == true) {
+                    return player;
+                }
+                else {
+                    throw new ArgumentException("Player not found");
+                }
+            }
+        }
+        public void AddPlayer(SoccerPlayer player) {
+            if (Players == null) {
+                players.Add(player);
+            }
+            else {
+                if (players.Contains(player)) {
+                    throw new ArgumentException("Player already on team");
+                }
+                else {
+                    if (player.Gender == TeamGender) {
+                        if (player.Age <= AgeLimit) {
+                            players.Add(player);
                         }
                         else {
-                            throw new ArgumentException("Not gender appropriate");
+                            throw new ArgumentException("Player too old for team");
                         }
+                    }
+                    else {
+                        throw new ArgumentException("Not gender appropriate");
                     }
                 }
             }
---
+++ Exercise4-SoccerTeam/UnitTestSportTeam/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise4SoccerTeam;
+using System.Collections.Generic;
+namespace UnitTestSportTeam {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestValidation1() {
+            SoccerPlayer sp = new SoccerPlayer();
+            Assert.AreEqual(sp.Name, "");
+            Assert.AreEqual(sp.Age, 0);
+            Assert.AreEqual(sp.Gender, Gender.Male);
+            Assert.AreEqual(sp.Position, Position.Defender);
+        }
+
+        [TestMethod]
+        public void TestValidation2() {
+            SoccerPlayer sp = new SoccerPlayer("Fergie", 22, Gender.Male, Position.Goalkeeper);
+            Assert.AreEqual(sp.Name, "Fergie");
+            Assert.AreEqual(sp.Age, 22);
+            Assert.AreEqual(sp.Gender, Gender.Male);
+            Assert.AreEqual(sp.Position, Position.Goalkeeper);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAgeLimit() {
+            SoccerPlayer sp = new SoccerPlayer("Fergie", 22, Gender.Male, Position.Goalkeeper);
+            SoccerTeam st = new SoccerTeam("Team 1", Gender.Male, 18);
+            st.AddPlayer(sp);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestGender() {
+            SoccerPlayer sp = new SoccerPlayer("Fergie", 22, Gender.Female, Position.Goalkeeper);
+            SoccerTeam st = new SoccerTeam("Team 1", Gender.Male, 18);
+            st.AddPlayer(sp);
+        }
+
+        [TestMethod]
+        public void TestAddPlayer1() {
+            SoccerPlayer sp1 = new SoccerPlayer("Fergie", 12, Gender.Male, Position.Goalkeeper);
+            SoccerPlayer sp2 = new SoccerPlayer("Sam", 12, Gender.Male, Position.Defender);      
+            SoccerTeam st = new SoccerTeam("Team 1", Gender.Male, 18);
+            st.AddPlayer(sp1);
+            st.AddPlayer(sp2);
+            CollectionAssert.Contains(st.Players, sp1);
+            CollectionAssert.Contains(st.Players, sp2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestAddPlayer2() {
+            SoccerPlayer sp1 = new SoccerPlayer("Fergie", 12, Gender.Male, Position.Goalkeeper);
+            SoccerTeam st = new SoccerTeam("Team 1", Gender.Male, 18);
+            st.AddPlayer(sp1);
+            st.AddPlayer(sp1);
+        }
+        [TestMethod]
+        public void TestIndexer() {
+            SoccerPlayer m1 = new SoccerPlayer("Tom", 12, Gender.Male, Position.Goalkeeper);
+            SoccerPlayer m2 = new SoccerPlayer("Sam", 12, Gender.Male, Position.Defender);
+            SoccerPlayer m3 = new SoccerPlayer("Fergie", 12, Gender.Male, Position.Goalkeeper);
+             
+            SoccerTeam pl = new SoccerTeam("Team 1", Gender.Male, 18);
+            pl.AddPlayer(m1);
+            pl.AddPlayer(m2);
+            pl.AddPlayer(m3); 
+
+            List<SoccerPlayer> team = new List<SoccerPlayer>();
+            team.Add(m1);
+           
+            // Indexer adding based on name
+            List<SoccerPlayer> nameList = new List<SoccerPlayer>();
+            foreach (SoccerPlayer m in pl["Tom"]) {
+                 nameList.Add(m);
+            }
+            // checks both lists are equal
+            CollectionAssert.AreEqual(team, nameList);
+        }
+    }
+}
